cf3a7d073093c59cde05dc630a6d5ae5407e57e3
AdilsonAr <cycles.250@gmail.com>
2020-10-27 00:03:49 -0600

    Generador de usuarios terminado

--- src/main/java/com/random/RandomApplication.java
+++ src/main/java/com/random/RandomApplication.java
@@ -1,14 +1,10 @@
 package com.random;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.random.model.Persona;
-import com.random.repository.PersonRepository;
+import com.random.service.PersonaService;
 
 @SpringBootApplication
 public class RandomApplication {
@@ -16,12 +12,9 @@ public class RandomApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(RandomApplication.class, args);
 		
-		PersonRepository repo= context.getBean(PersonRepository.class);
-		List<Persona> lista=new ArrayList<Persona>();
-		lista=repo.readNames01();
-		for (Persona qu:lista) {
-			System.out.println(qu.getName());
-		}
+		PersonaService serv = context.getBean(PersonaService.class);
+		System.out.print(serv.generate());
+		
 	}
 
 }
--- src/main/java/com/random/service/PersonaService.java
+++ src/main/java/com/random/service/PersonaService.java
@@ -4,19 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.random.model.Persona;
 import com.random.repository.PersonRepository;
 
+@Service
 public class PersonaService {
-/*
-	@Autowired
-	static PersonRepository repo;
 
-	public static void main(String[] args) {
-		List<Persona> lista=new ArrayList<Persona>();
-		for (Persona qu:lista) {
-			System.out.println(qu.getName());
+	// int valorEntero = Math.floor(Math.random()*(N-M+1)+M); // Valor entre M y N,
+	@Autowired
+	PersonRepository repo;
+
+	public String generate() {
+
+		String sql = "insert into usuario values ";
+		List<Persona> lista = new ArrayList<Persona>();
+		lista = repo.readNames01();
+		for (Persona qu : lista) {
+			sql += "(" + qu.getId() + "," + (qu.getName() + (int)Math.floor(Math.random() * 50 + 1)) + ","
+					+ (int)Math.floor(Math.random() * (9999999 - 1000000 + 1) + 1000000) + ","
+					+ (int)Math.floor(Math.random() * (3) + 1) + " ),\n";
 		}
-	}*/
+		sql += ";";
+		return sql;
+	}
+
 }
